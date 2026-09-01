cfda68fe67ad1267547a63e75982ecbac6c00965
d3n1zws <danizeh@code.edu.az>
2026-09-01 15:16:05 +0400

    added task1

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp4;
+using System.ComponentModel;
+
+Console.WriteLine("username daxil edin:");
+string username = Console.ReadLine();
+Console.WriteLine("password daxil edin:");
+string password = Console.ReadLine();
+ConsoleApp4.User user = new ConsoleApp4.User(username, password);
+Console.WriteLine($"username : {user.Username}");
+Console.WriteLine($"password : {user.Password}");
+Console.WriteLine("yas daxil edin:");
+int age = int.Parse(Console.ReadLine());
+user.Age = age;
+Console.WriteLine(user.Age);
---
+++ ConsoleApp4/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    internal class User
+    {
+        public string Username; 
+        public string Password
+        {
+            get
+            {
+                return field;
+            }
+            set
+            {
+                if (value == "" || value == null)
+                {
+                    Console.WriteLine("password bos ola bilmez");
+                    return;
+                }
+                bool check1 = false, check2 = false, check3 = false;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (char.IsUpper(value[i]))
+                        check1 = true;
+                    if (char.IsLower(value[i]))
+                        check2 = true;
+                    if (char.IsNumber(value[i]))
+                        check3 = true;
+                }
+                if (check1 && check2 && check3)
+                    field = value;
+                else
+                    Console.WriteLine("sertler odenmir");
+            }
+        }
+        public int Age
+        {
+            get;
+            set
+            {
+                if (value < 0)
+                    Console.WriteLine("yas 0-dan kicik ola bilmez");
+                field = value;
+            }
+        }
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
+
