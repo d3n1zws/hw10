88b32ae3aecc8094122d071b88528fcb19ea07c9
d3n1zws <danizeh@code.edu.az>
2026-09-01 17:19:11 +0400

    added task3

---
+++ ConsoleApp2/Calculation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Calculation
+    {
+        public int n { get; set; }
+        public int m { get; set; }
+        public char c 
+        { 
+            get; 
+            set
+            {
+                if (value == '+' || value == '-' || value == '*' || value == '/')
+                {
+                    field = value;
+                }
+                else
+                {
+                    Console.WriteLine("sehv operator daxil elemisiniz");
+                }
+            }
+        }
+        public Calculation(int n, int m, char c)
+        {
+            this.n = n;
+            this.m = m;
+            this.c = c;
+        }
+        public void Calculate()
+        {
+            switch (c)
+            {
+                case '+':
+                    Console.WriteLine($"Cavab: {n + m}");
+                    break;
+                case '-':
+                    Console.WriteLine($"Cavab: {n - m}");
+                    break;
+                case '*':
+                    Console.WriteLine($"Cavab: {n * m}");
+                    break;
+                case '/':
+                    if (m != 0)
+                    {
+                        Console.WriteLine($"Cavab: {(double)n / m}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("0-a bolme sehvdir");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("eded1 daxil edin");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("eded2 daxil edin");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("operator daxil edin");
+char c = char.Parse(Console.ReadLine());
+ConsoleApp2.Calculation calc = new ConsoleApp2.Calculation(n, m, c);
+calc.Calculate();
